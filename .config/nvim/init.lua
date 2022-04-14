8c8592e522d55f5087d00cd1ef421e329d3d2c72
SlowGahnoo <chrisme2093@protonmail.com>
2022-04-15 01:30:06 +0300

    Migrated from init.vim to init.lua

---
+++ .config/nvim/init.lua
@@ -0,0 +1,41 @@
+vim.g.mapleader        = " "
+vim.opt.termguicolors  = true
+vim.opt.mouse          = "a"
+vim.opt.undofile       = true
+vim.opt.shell          = "/bin/zsh"
+vim.opt.number         = true
+vim.opt.relativenumber = true
+vim.opt.clipboard      = "unnamedplus"
+
+vim.opt.tabstop     = 4
+vim.opt.shiftwidth  = 4
+vim.opt.softtabstop = 4
+vim.opt.autoindent  = true
+vim.opt.smartindent = true
+
+vim.opt.completeopt = "menu,menuone,noselect"
+
+vim.opt.ttimeoutlen = 100
+
+-- "Navigate by display lines instead of regular lines
+vim.api.nvim_set_keymap("", "j", "gj", {noremap = true})
+vim.api.nvim_set_keymap("", "k", "gk", {noremap = true})
+
+-- "Switch splits quickly
+vim.api.nvim_set_keymap("", "<c-j>", "<c-w>j", {noremap = true})
+vim.api.nvim_set_keymap("", "<c-k>", "<c-w>k", {noremap = true})
+vim.api.nvim_set_keymap("", "<c-h>", "<c-w>h", {noremap = true})
+vim.api.nvim_set_keymap("", "<c-l>", "<c-w>l", {noremap = true})
+
+-- " Open Terminal
+vim.api.nvim_set_keymap("n", "tt", ":split<Bar>terminal<CR>", {noremap = true})
+
+
+-- "Save me from misspressing shift
+vim.cmd("command! W :w")
+vim.cmd("command! Q :q")
+
+
+require('plugins')
+
+vim.cmd("colorscheme dracula")
---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,191 @@
+vim.cmd [[packadd packer.nvim]]
+
+require('packer').startup(function()
+	use 'wbthomason/packer.nvim'
+	use 'neovim/nvim-lspconfig'
+	use 'hrsh7th/cmp-nvim-lsp'
+	use 'hrsh7th/cmp-buffer'
+	use 'hrsh7th/nvim-cmp'
+	use 'hrsh7th/cmp-vsnip'
+	use 'hrsh7th/vim-vsnip'
+	use 'hrsh7th/vim-vsnip-integ'
+	use 'rafamadriz/friendly-snippets'
+	use 'nvim-orgmode/orgmode'
+	use {
+		'nvim-treesitter/nvim-treesitter',
+		run = ':TSUpdate'
+	}
+	use {
+		'turbio/bracey.vim',
+		run = 'npm --prefix server'
+	}
+	use 'windwp/nvim-autopairs'
+	use 'folke/which-key.nvim'
+	use 'norcalli/nvim-colorizer.lua'
+	use 'hoob3rt/lualine.nvim'
+	use 'mfussenegger/nvim-dap'
+	use 'rcarriga/nvim-dap-ui'
+	use 'mfussenegger/nvim-dap-python'
+	use 'terrortylor/nvim-comment'
+	use 'dracula/vim'
+	use {
+	    'kyazdani42/nvim-tree.lua',
+	    requires = {
+	      'kyazdani42/nvim-web-devicons', -- optional, for file icon
+	    }
+	}
+	use 'tikhomirov/vim-glsl'
+end)
+
+vim.cmd([[autocmd! BufNewFile,BufRead *.vs,*.fs,*.fragmentshader,*.vertexshader set ft=glsl]])
+
+require('nvim-tree').setup() 
+vim.api.nvim_set_keymap("n", "<c-f>", ":NvimTreeToggle<CR>", {noremap = true})
+
+require('nvim_comment').setup()
+
+require('lualine').setup({
+  options = {
+    theme = 'dracula', 
+    component_separators = {'|'},
+    section_separators = {''},
+  }
+})
+
+require('colorizer').setup({
+  css = {css = true;}
+})
+
+require('nvim-autopairs').setup()
+require("which-key").setup()
+require('orgmode').setup_ts_grammar()
+require('orgmode').setup()
+
+local cmp = require'cmp'
+
+require'nvim-treesitter.configs'.setup {
+  highlight = {
+    enable = true,
+    disable = {'org'},
+    additional_vim_regex_highlighting = {'org'},
+  },
+  ensure_installed = {'org'}, -- Or run :TSUpdate org
+}
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  
+  completion = {
+  	completeopt = 'menu,menuone,noinsert',
+  	autocomplete = false,
+  },
+
+  mapping = {
+    ['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+    ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+    ['<Down>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+    ['<Up>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<CR>'] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    })
+  },
+
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' },
+    { name = 'buffer' },
+    { name = 'orgmode' },
+  }
+})
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
+local servers = {
+    'clangd', 
+    'rust_analyzer', 
+    'jedi_language_server', 
+    'tsserver', 
+    'html', 
+    'cssls',
+    }
+
+for _, lsp in ipairs(servers) do
+    require('lspconfig')[lsp].setup {
+  	  on_attach = on_attach,
+  	  capabilities = capabilities,
+    }
+end
+
+
+require('dap')
+vim.fn.sign_define('DapBreakpoint', {text='', texthl='', linehl='', numhl=''})
+require('dap-python').setup('/usr/bin/python')
+
+require("dapui").setup({
+  icons = { expanded = "▾", collapsed = "▸" },
+  mappings = {
+    -- Use a table to apply multiple mappings
+    expand = { "<CR>", "<2-LeftMouse>" },
+    open = "o",
+    remove = "d",
+    edit = "e",
+    repl = "r",
+    toggle = "t",
+  },
+  sidebar = {
+    -- You can change the order of elements in the sidebar
+    elements = {
+      -- Provide as ID strings or tables with "id" and "size" keys
+      {
+        id = "scopes",
+        size = 0.25, -- Can be float or integer > 1
+      },
+      { id = "breakpoints", size = 0.25 },
+      { id = "stacks", size = 0.25 },
+      { id = "watches", size = 00.25 },
+    },
+    size = 40,
+    position = "left", -- Can be "left", "right", "top", "bottom"
+  },
+  tray = {
+    elements = { "repl" },
+    size = 10,
+    position = "bottom", -- Can be "left", "right", "top", "bottom"
+  },
+  floating = {
+    max_height = nil, -- These can be integers or a float between 0 and 1.
+    max_width = nil, -- Floats will be treated as percentage of your screen.
+    border = "single", -- Border style. Can be "single", "double" or "rounded"
+    mappings = {
+      close = { "q", "<Esc>" },
+    },
+  },
+  windows = { indent = 1 },
+})
+
+
+vim.cmd([[
+imap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'
+smap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'
+imap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'
+smap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'
+
+
+nnoremap <silent> <F5>       :lua require('dap').continue()<CR>
+nnoremap <silent> <leader>n  :lua require('dap').step_over()<CR>
+nnoremap <silent> <leader>si :lua require('dap').step_into()<CR>
+nnoremap <silent> <leader>so :lua require('dap').step_out()<CR>
+nnoremap <silent> <leader>b  :lua require('dap').toggle_breakpoint()<CR>
+nnoremap <silent> <leader>dr :lua require('dap').repl.toggle()<CR>
+nnoremap <silent> <leader>dd :lua require('dapui').toggle()<CR>
+]])
